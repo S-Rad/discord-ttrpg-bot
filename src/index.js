e22febeab7fcbea5c3fa681ea1e8b7a62d61ca34
S-Rad <42512223+S-Rad@users.noreply.github.com>
2021-05-31 23:20:43 +0200

    add games over discord, list games

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const config = require("./config.json");
 const client = new Discord.Client();
 let games = {table:[]}
 const fs = require('fs');
+const regexp = new RegExp(',', 'g');
 
 
 client.once('ready', () => {
@@ -12,6 +13,7 @@ client.once('ready', () => {
 
 client.on('message', message => {
     let text;
+    
 
     if(message.content.charAt(0) == config.prefix){
         text = message.content.slice(1, message.content.length);
@@ -22,12 +24,27 @@ client.on('message', message => {
             message.channel.send('Pong.');
         }
         if(text.startsWith('add')){
-            addGame(text);
+            if((text.match(/,/g) || []).length != 4)
+            {
+                message.channel.send('Syntax: !add <Name>, <System>, <Startzeit>, <Endzeit>, <Max. Spieleranzahl>');
+            }
+            else{
+                addGame(text, message.author);
+            }
+
         }
 
         if(text.startsWith('reset')){
             resetGames();
             updateJSON();
+        }
+
+        if(text.startsWith('list')){
+            listGames(message.channel);
+        }
+
+        if(text.startsWith('show')){
+            showGame(message.content.slice(6, message.content.length), message.channel);
         }
 
     }
@@ -38,7 +55,7 @@ const loadJSON = () => {
         if (err){
             console.log("Error in loading: " + err);
         } else {
-            console.log("Parsing data: "+ data);
+            //console.log("Parsing data: "+ data);
         games = JSON.parse(data); //now it an object
     }});
 
@@ -54,10 +71,8 @@ const updateJSON = () => {
 
 }
 
-const addGame = (text) => {
-    let game = {
-        "text": text
-    }
+const addGame = (text, author) => {
+    let game = createGameObject(text,  author.tag);
 
     fs.readFile('games.json', 'utf8', function readFileCallback(err, data){
         if (err){
@@ -74,10 +89,79 @@ const resetGames = () => {
     console.log("Table content: " + games.table);
 }
 
+const createGameObject = (args, author) => {
+
+    let text = args.slice(4, args.length);
+
+    let game =  {
+        "id": games.table.length,
+        "name": '*leer*',
+        "leiter": author,
+        "system": '*leer*',
+        "start": '*leer*',
+        "end": '*leer*',
+        "maxplayers": '*leer*',
+        "players": '*leer*',
+        "notes": '*leer*'
+    };
+    game.name = text.slice(0, text.indexOf(',')); 
+    text = text.slice(text.indexOf(',')+1);
+    game.system = text.slice(0, text.indexOf(',')); 
+    text = text.slice(text.indexOf(',')+1);
+    game.start = text.slice(0, text.indexOf(',')); 
+    text = text.slice(text.indexOf(',')+1);
+    game.end = text.slice(0, text.indexOf(',')); 
+    text = text.slice(text.indexOf(',')+1);
+    game.maxplayers = text.slice(0, text.indexOf(',')); 
+    text = text.slice(text.indexOf(',')+1);
+
+    return game;
+
+
+}
+
 
 const deleteGame = (id) => {
   
 }
+
+const listGames = (channel) => {
+    let listString = "";
+    games.table.forEach(game => {
+        listString = listString + game.id + ", " + game.name + ", " + game.leiter + ", " + game.system + ", " + game.start + "\n";
+        //message.channel.send(game.name + ", " + game.leiter + ", " + game.system + ", " + game.start);
+    });
+    channel.send(listString);
+}
+
+const showGame = (id, channel) => {
+    let game;
+    if(!isNumeric(id) || id > games.table.length || id < 0){
+        console.log(id);
+        channel.send("Syntax: !show <zahl>. Finde dein Spiel mit !list");
+    }
+    else{
+        game = findGame(id);
+        console.log(game);
+        channel.send(game);
+    }
+    
+}
+
+const findGame = (id) => {
+    games.table.forEach(game => {
+        if(game.id == id){
+            console.log(game);
+            return game;
+        }
+    })
+} 
+
+function isNumeric(str) {
+    if (typeof str != "string") return false // we only process strings!  
+    return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+           !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
+  }
 
 client.login(config.token);
 
